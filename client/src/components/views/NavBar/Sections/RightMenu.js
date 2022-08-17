a4afad37409277eb0440ddc388f9d61790eeefaf
unknown <joonsun926@gmail.com>
2022-08-18 01:26:28 +0900

    update

--- client/src/components/views/NavBar/Sections/RightMenu.js
+++ client/src/components/views/NavBar/Sections/RightMenu.js
@@ -33,8 +33,7 @@ function RightMenu(props) {
   } else {
     return (
       <Menu mode={props.mode}>
-        <Menu.Item key="upload">
-          <a href="/product/upload">Upload</a>
+
         </Menu.Item>
         <Menu.Item key="logout">
           <a onClick={logoutHandler}>Logout</a>
